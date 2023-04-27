a9f7ed9461efa479eefd3574b69b42642c6cb023
SandhyaRaniAddanki <131636275+SandhyaRaniAddanki@users.noreply.github.com>
2023-04-27 22:55:54 +0530

    Add files via upload

---
+++ example.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+     int a,b,c,d;
+     
+     printf("enter a value:");
+     scanf("%d",&a);
+     printf("enter a value:")
+     scanf("%d",&b);
+     c=a%b;
+     d=a/b;
+     printf("The number of friends in each team is ",%d," and left out is",%d, &a,&b);
+     }
